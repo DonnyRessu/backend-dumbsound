78e95253e660cdb73628b619d1841efceb8974bc
Donny Erlambang Ressu <Donny Erlambang>
2023-06-19 00:27:28 +0700

    update models

--- models/music.go
+++ models/music.go
@@ -3,7 +3,7 @@ package models
 type Music struct {
 	ID       int    `json:"id" form:"id" gorm:"auto_increment:primary_key"`
 	Title    string `json:"title" form:"title" gorm:"type: varchar(255)"`
-	Year     int    `json:"year" form:"year" gorm:"type: varchar(255)"`
+	Year     int    `json:"year" form:"year"`
 	Image    string `json:"image" form:"image" gorm:"type: varchar(255)"`
 	Song     string `json:"song" form:"song" gorm:"type: varchar(255)"`
 	ArtistID int    `json:"artist_id" form:"artist_id" gorm:"type: int"`
